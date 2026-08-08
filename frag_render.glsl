17e4add5545fc6e5d02825ae9e0a6fd2531531f6
Ray Chen <tankimarshal2@gmail.com>
2026-08-08 21:02:46 +0100

    Add colour to pixels

--- frag_render.glsl
+++ frag_render.glsl
@@ -2,9 +2,18 @@
 precision highp float;
 
 uniform sampler2D uTexture;
+uniform float rest;
 in vec2 texCoord;
 out vec4 fragColor;
 
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main() {
-    fragColor = vec4(texture(uTexture, texCoord).rrr, 1.0);
+    float pixel = texture(uTexture, texCoord).r;
+    vec3 hsv = vec3(mix(0.666, 0.333, pow(pixel, 2.0)), mix(0.7, 1.0, pixel), pixel);
+    fragColor = vec4(hsv2rgb(hsv), 1.0);
 }
