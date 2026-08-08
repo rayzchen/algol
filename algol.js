fedc8e31a35d306da44e3c1ea9f7c026afa1dd4d
Ray Chen <tankimarshal2@gmail.com>
2026-08-08 13:17:27 +0100

    Add Texture class and set up vbo + vao

--- algol.js
+++ algol.js
@@ -1,6 +1,58 @@
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("gl-canvas");
-const gl = canvas.getContext("webgl");
+const gl = canvas.getContext("webgl2");
+
+class Texture {
+    constructor(width, height, fill=0) {
+        this.width = width;
+        this.height = height
+
+        const pixels = new Uint8Array(width * height);
+        if (fill != 0) {
+            for (let i = 0; i < width * height; i++) {
+                if (Math.random() < fill) {
+                    pixels[i] = 255;
+                }
+            }
+        }
+
+        this.texture = gl.texImage2D(
+            gl.TEXTURE_2D,
+            0,
+            gl.LUMINANCE,
+            width,
+            height,
+            border,
+            0,
+            gl.LUMINANCE,
+            gl.UNSIGNED_BYTE,
+            pixels
+        );
+
+        this.framebuffer = gl.createFramebuffer();
+        gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, this.texture, 0);
+    }
+
+    bindTexture() {
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    }
+
+    renderTo() {
+        gl.bindFramebuffer(this.framebuffer);
+        gl.viewport(0, 0, this.width, this.height);
+    }
+}
+
+const vertices = new Float32Array([
+    -1.0, -1.0, 0.0,
+    1.0,-1.0, 0.0,
+    1.0, 1.0, 0.0,
+    -1.0,-1.0, 0.0,
+    1.0, 1.0, 0.0,
+    -1.0, 1.0, 0.0
+])
 
 function main() {
     if (gl == null) {
@@ -10,6 +62,15 @@ function main() {
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    const vbo = gl.createBuffer();
+    const vao = gl.createVertexArray();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+    gl.bindVertexArray(vao);
+
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 3 * vertices.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(0);
 }
 
 main();
